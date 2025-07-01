036366d50cd8f49c9e7736d03ae1ba6855a2dd1e
Abhishekrao980 <ay9802827@gmail.com>
2025-07-01 13:21:40 +0530

    adding .gitignore

---
+++ frontend/src/Components/Chart/Chart.js
@@ -0,0 +1,69 @@
+import React from "react";
+import {
+  Chart as ChartJs,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+  ArcElement,
+} from "chart.js";
+
+import { Line } from "react-chartjs-2";
+import styled from "styled-components";
+import { useGlobalContext } from "../../context/globalContext";
+import { dateFormat } from "../../utils/dateFormat";
+
+ChartJs.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+  ArcElement
+);
+
+function Chart() {
+  const { incomes, expenses } = useGlobalContext();
+
+  const allDates = [
+    ...new Set([
+      ...incomes.map((inc) => dateFormat(inc.date)),
+      ...expenses.map((exp) => dateFormat(exp.date)),
+    ]),
+  ].sort();
+
+  const data = {
+    labels: allDates,
+    datasets: [
+      {
+        label: "Income",
+        data: allDates.map((date) => {
+          const income = incomes.find((inc) => dateFormat(inc.date) === date);
+          return income ? income.amount : 0;
+        }),
+      },
+    ],
+  };
+
+  return (
+    <ChartStyled>
+      <Line data={data} />
+    </ChartStyled>
+  );
+}
+
+const ChartStyled = styled.div`
+  background: #fcf6f9;
+  border: 2px solid #ffffff;
+  box-shadow: 0px 1px 15px rgba(0, 0, 0, 0.06);
+  padding: 1rem;
+  border-radius: 20px;
+  height: 100%;
+`;
+
+export default Chart;
