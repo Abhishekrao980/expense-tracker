aaeb735a42edeee2adb347aa68451534485b80f9
Abhishekrao980 <ay9802827@gmail.com>
2025-07-01 15:17:00 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -8,12 +8,17 @@ require("dotenv").config();
 
 const PORT = process.env.PORT;
 
+
 //Middlewares
 app.use(express.json());
 app.use(cors());
 
 //Routes
 readdirSync('./routes').map((route)=>app.use('/api/v1',require('./routes/'+ route )))
+
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
 
 
 const server = () => {
